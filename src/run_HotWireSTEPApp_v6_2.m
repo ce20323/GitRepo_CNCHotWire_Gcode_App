31510e4f3033a764ace0ae516557bcaccccb12ef
ce20323 <ce20323@bristol.ac.uk>
2025-12-09 13:27:14 +0000

    added run code

---
+++ src/run_HotWireSTEPApp_v6_2.m
@@ -0,0 +1,8 @@
+function run_HotWireSTEPApp_v6_2
+% Simple launcher for the HotWire STEP App v6.2
+%
+% Usage (from Command Window):
+%   run_HotWireSTEPApp_v6_2
+
+app = HotWireSTEPApp_v6_2(); %#ok<NASGU>
+end
